cfbd2b7a24e21eb5567daaa5d35d56148bf34fdc
worladev <confidenceworlasi@gmail.com>
2024-07-14 23:40:15 +0000

    gathering models implementation logic

--- folio/models.py
+++ folio/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+class Bio(models.Model):
+    # first_name - char field
+    # middle_name - char field
+    # last_name - char field
+    # headline - char field
+    # introduction - text field
+    # profile_image - image field
+    # resume - file upload
+    pass
+
+
+class Projects(models.Model):
+    # title - char field
+    # project_image - image field
+    # technologies_used - text field
+    # project url - url field
+    # date_created - date field
+    pass
+
+
+class Education(models.Model):
+    # school_name - char field
+    # course - char field
+    # start_date - date field
+    # end_date - date field
+    pass
+
+
+class WorkExperience(models.Model):
+    # organization - char field
+    # position_held - char field
+    # duties - text field
+    # start_date - date field
+    # end_date - date field
+    pass
+
+
+class Certification(models.Model):
+    # name - char field
+    pass
+
+
+class Contact(models.Model):
+    # email - email field
+    # cell_phone1 - integer field
+    # cell_phone2 - integer field
+    pass
