048a54a5be715ecac85378008b24b4e8c7cd343b
worladev <confidenceworlasi@gmail.com>
2024-10-14 15:01:20 -0400

    added a custom field class for comma separated tags

---
+++ worlafolio/folio/fields.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.forms import CharField
+from typing import List, Union, Optional
+
+
+# Custom comma separated tags field
+class CustomTagsField(models.Field):
+    description = "A custom field for storing a list of tags as a comma-separated-string."
+
+    def _helper(self, value: Optional[Union[str, list]]) -> List[str]:
+        """A helper method to convert string or list to a list of stripped strings"""
+        if isinstance(value, list):
+            return [x.strip() for x in value]
+        if value is None:
+            return []
+        return [x.strip() for x in value.split(',')]
+    
+    def from_db_value(self, value: Optional[str], expression, connection) -> List[str]:
+        """Convert the database value to a Python list."""
+        return self._helper(value)
+    
+    def to_python(self, value: Optional[Union[str, list]]) -> List[str]:
+        """Convert the input value to a Python list."""
+        return self._helper(value)
+    
+    def get_prep_value(self, value: Optional[List[str]]) -> str:
+        """Prepare the value for storage in the database."""
+        if value is None:
+            return ''
+        return ','.join(str(x) for x in value)
+    
+    def db_type(self, connection) -> str:
+        """Specify the database type for this field."""
+        if connection.settings_dict['ENGINE'] == 'django.db.backend.postgresql':
+            return 'text'
+        elif connection.settings_dict['ENGINE'] == 'django.db.backend.sqlite3':
+            return 'text'
+        return "char(%s)" % self.max_length
+    
+    def formfield(self, **kwargs):
+        """Return a CharField for form use."""
+        defaults = {'form_class': CharField}
+        defaults.update(kwargs)
+        return super().formfield(**defaults)
+    
