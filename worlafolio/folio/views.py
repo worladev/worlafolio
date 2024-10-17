f3f3233cd96bf267f110eb643bbc5ae335856d12
worladev <confidenceworlasi@gmail.com>
2024-10-16 22:00:27 -0400

    first view for the index template

--- worlafolio/folio/views.py
+++ worlafolio/folio/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from folio.models import User
 
 # Create your views here.
+def index(request):
+    user = User.objects.all()
+    context = {
+        "user": user,
+    }
+    return render(request, "folio/index.html", context)
