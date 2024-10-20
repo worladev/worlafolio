e88423d74a280de597ed49084d0dec070ae73bbd
worladev <confidenceworlasi@gmail.com>
2024-10-20 02:25:42 -0400

    initial UI

--- worlafolio/worlafolio/settings.py
+++ worlafolio/worlafolio/settings.py
@@ -118,7 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+# STATICFILES_DIRS = [BASE_DIR / 'folio/static',]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 
 MEDIA_URL = 'media/'
